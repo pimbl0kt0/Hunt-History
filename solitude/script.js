a13499838023278b77d372c5c18824bb964468e5
Ioannis <io.a.skoul@gmail.com>
2020-05-05 19:07:10 -0400

    Working on solitude page

--- solitude/script.js
+++ solitude/script.js
@@ -20,6 +20,6 @@ function clickLocations()
 
 function initMap() {
     var solitude = {lat: -25.344, lng: 131.036};
-    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: solitude});
+    var map = new google.maps.Map($("#map").get(0), {zoom: 4, center: solitude});
     var marker = new google.maps.Marker({position: solitude, map: map});
 }
