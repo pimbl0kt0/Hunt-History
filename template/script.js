981373d4b0dfc9bda4ccb6ecc7e032eec603bb4b
Ioannis <io.a.skoul@gmail.com>
2020-05-05 20:06:39 -0400

    Template for site markers started

---
+++ template/script.js
@@ -0,0 +1,25 @@
+resetLayout();
+
+window.addEventListener('resize', (e) =>
+{
+    resetLayout();
+});
+
+function resetLayout()
+{
+    $("#positioner").css({width: $("body").width()-$("#sidebar").width(), 'margin-left': $("#sidebar").width()});
+}
+
+function clickLocations()
+{
+    $("#locations").css("visibility", "visible");
+    $("#locations").css("height", "auto");
+    $("#locations").css("width", "auto");
+    resetLayout();
+}
+
+function initMap() {
+    var solitude = {lat: -25.344, lng: 131.036};
+    var map = new google.maps.Map($("#map").get(0), {zoom: 4, center: solitude});
+    var marker = new google.maps.Marker({position: solitude, map: map});
+}
